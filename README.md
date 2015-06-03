071579867be877d14aeeea7fbae48bdef1b4a49b
hirotaka <hirotakaster@gmail.com>
2015-06-04 00:05:22 +0900

    update for Spark photon.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 MQTT for Spark
 ============
 
-<a href="http://mqtt.org/" target=_blank>MQTT</a> library for Spark core version 0.2.0.
+<a href="http://mqtt.org/" target=_blank>MQTT</a> library for Spark core version 0.2.4.
 
 Example mqtttest.ino is dependent on:
 
--- firmware/MQTT.h
+++ firmware/MQTT.h
@@ -4,8 +4,7 @@ MQTT library for Spark core
 Version 0.1: initial version
 
 Copyright (c) 2014 Hirotaka Niisato
-Per
-mission is hereby granted, free of charge, to any person obtaining
+Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
 "Software"), to deal in the Software without restriction, including
 without limitation the rights to use, copy, modify, merge, publish,
